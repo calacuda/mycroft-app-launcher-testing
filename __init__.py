4f0c9bd661808c88e8ab883decd610da2e1c5381
calacuda <eowest@gmail.com>
2020-10-10 19:13:57 -0400

    testing repl

--- __init__.py
+++ __init__.py
@@ -114,7 +114,7 @@ class Launcher(MycroftSkill):
         run('notify-send "debug" "open repl called"')
         self.lines_in = 0
         term = self.settings.get("terminal")
-        run('notify-send "debug" "term :  {term}"')
+        run(f'notify-send "debug" "term :  {term}"')
         spawn(term + " -e " + lang, self.read_term)
         
     def read_term(self, fd):
