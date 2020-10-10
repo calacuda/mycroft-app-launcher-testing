1d20e52fb59228e226c3d767e0c43343ff9cfb7b
calacuda <eowest@gmail.com>
2020-10-10 19:09:08 -0400

    testing repl

--- __init__.py
+++ __init__.py
@@ -123,7 +123,7 @@ class Launcher(MycroftSkill):
         data = read(fd, 1024)
         if self.lines_in > 0:
             run('notify-send "debug" "after header"')
-            self.speak(data)
+            self.speak(data.decode("utf-8"))
             run('notify-send "debug" "after speak"')
             #print(data.decode("utf-8"))
             pass
